3416f0fdff7708840d71ca5569bf818ce0ff67aa
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2023-10-18 16:12:02 +0800

    chore: remove log

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { orders } from '@/public/data'
 import OrderHistory from './components/OrderHistory'
 
 export default function Home() {
-  console.log('history', orders);
   return (
     <div className='flex items-center justify-center min-h-screen py-10'>
       <OrderHistory />
